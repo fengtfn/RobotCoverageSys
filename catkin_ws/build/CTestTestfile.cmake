ec696f9f1490df45fed4336ebf914d3e8f2cca3e
fengteng <fengt@qfeeltech.com>
2019-11-19 10:16:40 +0800

    test

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tfn/git-project/RobotCoverageSys/catkin_ws/src
+# Build directory: /home/tfn/git-project/RobotCoverageSys/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/tfn/git-project/RobotCoverageSys/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/tfn/catkin_ws/devel".split(';') if "/home/tfn/catkin_ws/devel" != "" else []
