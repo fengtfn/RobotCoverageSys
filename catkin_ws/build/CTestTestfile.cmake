1034a9a1586c8a039b21a5fa44f31aa4966ae522
fengteng <fengt@qfeeltech.com>
2019-11-25 13:51:48 +0800

    rm useless file

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tfn/git-project/RobotCoverageSys/catkin_ws/src
-# Build directory: /home/tfn/git-project/RobotCoverageSys/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
